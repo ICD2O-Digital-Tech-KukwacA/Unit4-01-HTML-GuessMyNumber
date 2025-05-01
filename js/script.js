f936c49a01d213f8be178683a47ca7974797d1af
Kukwac Adiebo <kukwac.adiebo@stu.ocsb.ca>
2025-05-01 15:56:56 +0000

    Unit4-01

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mr. Kukwac
+// Created on: May 2025
 // This file contains the JS functions for index.html
+"use strict"
+
+function guessGame() {
+    const maxNumber = 6;
+    const minNumber = 1;
+    let randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
+    let display = "";
+    let userGuess = parseInt(document.getElementById("userGuess").value);
+    if (userGuess == randomNumber) {
+        display = "Correct! You guessed the number!";
+    }
+    if (userGuess != randomNumber) {
+    display = "Incorrect! The number was " + randomNumber + ".";
+    }
+    document.getElementById("result").innerHTML = display; ;
+}
